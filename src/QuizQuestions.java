cd8fa5c1cb1961585f8f7126773b065e36d10a4f
Gabriel <gabrielnino@edu.unifil.br>
2022-10-04 13:48:12 -0300

    nova att

--- src/QuizQuestions.java
+++ src/QuizQuestions.java
@@ -9,19 +9,16 @@ public class QuizQuestions {
 
         int pontos = 0;
 
-        System.out.println("====== QUIZ DO NINO V.1.0 ======");
-        System.out.println("1. Iniciar o jogo | 2. Regras | 3. Desenvolvedor | 4. Sair");
-        int escolha = sc.nextInt();
+        System.out.println("=================== QUIZ DO NINO V.1.0 ===================");
+        String restart;
+        do {
+            System.out.println("1. Iniciar o jogo | 2. Regras | 3. Desenvolvedor | 4. Sair");
+            int escolha = sc.nextInt();
 
-        switch (escolha) { // Aqui o programa faz com o que o usuario escolha um caso de 1 a 4.
-            case 1 -> {
+            switch (escolha) { // Aqui o programa faz com o que o usuario escolha um caso de 1 a 4.
+                case 1 -> {
 
-
-                String restart;
-                do {
-
-                    System.out.println("===== Quiz do Nino V.1.0 =====");
-                    System.out.println("Questão 1");
+                    System.out.println("======================= Questão 1 ======================");
                     System.out.println("Quantos são 2 + 2 ?");
                     System.out.println("A - 2");
                     System.out.println("B - 1");
@@ -33,7 +30,7 @@ public class QuizQuestions {
                         pontos += 10;
                     }
 
-                    System.out.println("Questão 2");
+                    System.out.println("======================= Questão 2 ======================");
                     System.out.println("Quantos são 4 + 2 ?");
                     System.out.println("A - 2");
                     System.out.println("B - 1");
@@ -45,7 +42,7 @@ public class QuizQuestions {
                         pontos += 10;
                     }
 
-                    System.out.println("Questão 3");
+                    System.out.println("======================= Questão 3 ======================");
                     System.out.println("Quantos são 8 + 2 ?");
                     System.out.println("A - 2");
                     System.out.println("B - 1");
@@ -61,22 +58,38 @@ public class QuizQuestions {
                     System.out.print("Deseja jogar novamente ? (s/n): ");
                     restart = sc.next();
 
-                } while (restart.equals("s"));
 
+                }
+                case 2 -> {
+                    System.out.println("");
+                    System.out.println("Regras do jogo");
+                    System.out.println("É um quiz de 3 questões com cada questão valendo 10pts");
+                    System.out.println("Voce tera alternativas de [a] a [d] e terá que escolher uma dentre elas para prosseguir no jogo");
+                    System.out.println("No término do quiz, voce tera uma visão da sua pontuação total");
+                    System.out.print("Deseja jogar novamente ? (s/n): ");
+                    restart = sc.next();
+                }
+                case 3 -> {
+                    System.out.println("");
+                    System.out.println("Meu nome é Gabriel, sou o desenvolvedor desse Quiz, tenho como objetivo colocar em prático o que estou aprendendo em programação. Por isso, proponho esse quiz simples de estruturas if else e switch case.");
+                    System.out.println("Version 1.0.0");
+                    System.out.print("Deseja jogar novamente ? (s/n): ");
+                    restart = sc.next();
+                }
+                case 4 -> {
+                    System.out.println("");
+                    System.out.print("Tem certeza que deseja sair ? (s/n): ");
+                    restart = sc.next();
+                }
+                default -> {
+                    System.out.println("");
+                    System.out.println("Voce inseriu um valor invalido");
+                    System.out.print("Deseja voltar ao menu principal ?(s/n): ");
+                    restart = sc.next();
+                }
             }
-            case 2 -> {
-                System.out.println("Regras do jogo");
-                System.out.println("É um quiz de 3 questões com cada questão valendo 10pts");
-                System.out.println("Voce tera alternativas de [a] a [d] e terá que escolher uma dentre elas para prosseguir no jogo");
-                System.out.println("No término do quiz, voce tera uma visão da sua pontuação total");
-            }
-            case 3 -> {
-                System.out.println("Meu nome é Gabriel, sou o desenvolvedor desse Quiz, tenho como objetivo colocar em prático o que estou aprendendo em programação. Por isso, proponho esse quiz simples de estruturas if else e switch case.");
-                System.out.println("Version 1.0.0");
-            }
-            case 4 -> System.out.println("Até a próxima");
-            default -> System.out.println("Insira um valor válido para jogar");
-        }
+        } while (restart.equals("s"));
+        System.out.println("");
         System.out.println("Obrigado por jogar :)");
         sc.close();
     }
